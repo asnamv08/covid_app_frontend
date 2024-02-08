97a21280947318af53e1c6c344a2c3070df27a78
asnamv08 <asnamvarghese001@gmail.com>
2024-02-08 22:14:52 +0530

    covid app frontend

---
+++ lib/pages/search.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class searchpatient extends StatelessWidget {
+  const searchpatient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
